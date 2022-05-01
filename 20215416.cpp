641e873f21ee26e3b39923be19887a9dbc47fc58
Leon-Leyang <scylh6@nottingham.edu.cn>
2022-05-01 20:34:18 +0800

    Implement best fit heuristic

--- 20215416.cpp
+++ 20215416.cpp
@@ -38,6 +38,7 @@ private:
     vector<Item> items;
 public:
     // Constructor
+    Bin();
     Bin(int id, int cap);
 
     // Getters and setters
@@ -80,7 +81,10 @@ class Solution{
 private:
     int binNum;
     int obj;
-    vector<Bin> bins;
+    // Unfilled bins
+    vector<Bin> ufBins;
+    // Filled bins
+    vector<Bin> fBins;
     const Problem& problem;
 public:
     // Constructor
@@ -91,9 +95,11 @@ public:
 
     // Getters and setters
     int getBinNum() const;
-    vector<Bin> getBins() const;
+    vector<Bin> getUFBins() const;
+    vector<Bin> getFBins() const;
     void setBinNum(int binNum);
-    void setBins(vector<Bin> bins);
+    void setUFBins(vector<Bin> ufBins);
+    void setFBins(vector<Bin> fBins);
 };
 
 // Class managing io related operations
@@ -124,27 +130,31 @@ public:
     virtual Solution genSolution(const Problem& problem) = 0;    
 
     // Function to compare volumns of two items
-    // Used to sort the unpacked items
+    // Used to sort the unpacked items in a descending order
     static bool compareItem(Item item1, Item item2);   
+
+    // Function to compare capacity left of two bins
+    // Used to sort the unfilled bins in an ascending order
+    static bool compareBin(Bin bin1, Bin bin2);   
 };
 
 // Class for best fit
-// class BestFitHeuristic{
-// public:
-//     // Function to generate solution with Best Fit heuristic 
-//     virtual Solution genSolution(const Problem& problem);
-// }
+class BFHeuristic: public Heuristic{
+public:
+    // Function to generate solution with Best Fit heuristic 
+    virtual Solution genSolution(const Problem& problem);
+};
 
 
 // Class for minimum bin slack(MBS) heuristic
-class [[deprecated]] MBSHeuristic: public Heuristic{
-public:
-    // Function to generate solution with MBS heuristic 
-    virtual Solution genSolution(const Problem& problem);
-private:
-    // Function to find the maximum packing given a restricted capacity and a vector of unpacked items in a descending order of volumn
-    map<int, vector<Item>> findMaxPack(int cap, vector<Item> uItems);
-};
+// class MBSHeuristic: public Heuristic{
+// public:
+//     // Function to generate solution with MBS heuristic 
+//     virtual Solution genSolution(const Problem& problem);
+// private:
+//     // Function to find the maximum packing given a restricted capacity and a vector of unpacked items in a descending order of volumn
+//     map<int, vector<Item>> findMaxPack(int cap, vector<Item> uItems);
+// };
 
 
 // Class solving the problem
@@ -201,6 +211,7 @@ int main(int argc, char* argv[]){
     for(int i = 0; i < problems.size(); i++){
         Solution solution = solver.solve(problems[i]);
         solutions.push_back(solution);
+        cout << "gap: " << solution.getAbsGap() << endl;
     }
 
     // Solution solution = solver.solve(problems[0]);
@@ -243,6 +254,7 @@ bool Item::operator ==(const Item& item){
 
 
 // Constructor for Bin
+Bin::Bin(){}
 Bin::Bin(int id, int cap){
     this->id = id;
     this->cap = cap;
@@ -260,14 +272,12 @@ int Bin::getCap() const{
 vector<Item> Bin::getItems() const{
     return items;
 }
-void Bin::setCapLeft(int capLeft){
-    this->capLeft = capLeft;
-}
 void Bin::setCap(int cap){
     this->cap = cap;
 }
 void Bin::addItem(Item item){
     this->items.push_back(item);
+    capLeft -= item.getVol();
 }
 
 
@@ -310,14 +320,19 @@ Solution::Solution(const Problem& problem): problem(problem){}
 int Solution::getBinNum() const{
     return binNum;
 }
-vector<Bin> Solution::getBins() const{
-    return bins;
+vector<Bin> Solution::getUFBins() const{
+    return ufBins;
 }
-void Solution::setBinNum(int binNum){
-    this->binNum = binNum;
+vector<Bin> Solution::getFBins() const{
+    return fBins;
 }
-void Solution::setBins(vector<Bin> bins){
-    this->bins = bins;
+void Solution::setUFBins(vector<Bin> ufBins){
+    this->ufBins = ufBins;
+    binNum = ufBins.size() + fBins.size();
+}
+void Solution::setFBins(vector<Bin> fBins){
+    this->fBins = fBins;
+    binNum = ufBins.size() + fBins.size();
 }
 
 //Function to calculate the absolute gap between the solution's bin number and optimal
@@ -416,119 +431,188 @@ char* IOManager::getCmdArg(char ** begin, char ** end, const string & arg){
         return 0;
 }
 
-
-// Function to generate solution with Best Fit heuristic 
-// Solution BestFitHeuristic::genSolution(const Problem& problem){
-//     vector<Bin> bins;
-
-// }
-
-
-
-
-
-
-
 // Function to compare volumns of two items
-// Used to sort the unpacked items
+// Used to sort the unpacked items in an descending order
 bool Heuristic::compareItem(Item item1, Item item2){
     return (item1.getVol() > item2.getVol());
 }
 
+// Function to compare capacity left of two bins
+// Used to sort the unfilled bins in an ascending order
+bool Heuristic::compareBin(Bin bin1, Bin bin2){
+    return (bin1.getCapLeft() < bin2.getCapLeft());
+}
 
-// Function to generate solution with MBS heuristic 
-Solution MBSHeuristic::genSolution(const Problem& problem){
-    vector<Bin> bins;
+// Function to generate solution with Best Fit heuristic 
+Solution BFHeuristic::genSolution(const Problem& problem){
+    // Initialize vector that stores unfilled bins in the ascending order of left capacity
+    vector<Bin> ufBins;
+    vector<Bin> fBins;
 
     // Initialize the solution
     Solution solution(problem);
-
+    
     // Get the items in the problem
     vector<Item> items = problem.getItems();
 
-    // Initialize the unpacked items
     vector<Item> uItems = items;
 
     // Sort the unpacked items in a descending order of volumn
     sort(uItems.begin(), uItems.end(), Heuristic::compareItem);
 
-    // Pack the items according to MBS until all items are packed
     int binNum = 0;
+    // Pack the items according to BF until all items are packed
     while(uItems.size() > 0){
-        Bin bin(binNum, problem.getBinCap());
-        binNum++;
+        Item item = uItems[0];
 
-        map<int, vector<Item>> maxPacking = findMaxPack(bin.getCapLeft(), uItems);
-        auto it = maxPacking.begin();
-        int maxVol = it->first;
-        vector<Item> pItems = it->second;
-        bin.setCapLeft(bin.getCapLeft() - maxVol);
-        for(Item item : pItems){
+        int i = 0;
+        Bin bin;
+
+        if(ufBins.size() == 0){ // If currently no unfilled bin available
+            Bin newBin(binNum, problem.getBinCap());
+            bin = newBin;
+            binNum++;
             bin.addItem(item);
+            ufBins.push_back(bin);
+            i = ufBins.size() - 1;
 
-	        auto it2 = find(uItems.begin(), uItems.end(), item);
-	        int index = it2 - uItems.begin();
-	        uItems.erase(uItems.begin() + index);
+        }else{  // Else find the bin with the smallest left capacity that can pack the item
+            for(i = 0; i < ufBins.size(); i++){
+                bin = ufBins[i];
+                if(bin.getCapLeft() >= item.getVol()){  // If there is unfilled bin that can pack the item
+                    bin.addItem(item);
+                    ufBins[i] = bin;
+                    break;
+                }
+            }
+            if(i == ufBins.size()){ // If there is no unfilled bin that is big enough to pack the item
+                Bin newBin(binNum, problem.getBinCap());
+                bin = newBin;
+                binNum++;
+                bin.addItem(item);
+                ufBins.push_back(bin);
+                i = ufBins.size() - 1;
+            }
         }
 
-        bins.push_back(bin);
+        // Remove the item from unpacked items
+        uItems.erase(uItems.begin() + 0);
+
+        // If the unfilled bin is now filled
+        // Move it to filled bins
+        if(bin.getCapLeft() == 0){
+            fBins.push_back(bin);
+	        ufBins.erase(ufBins.begin() + i);
+        }else{// Else sort the unfilled items in an ascending order of capacity left
+            sort(ufBins.begin(), ufBins.end(), Heuristic::compareBin);
+        }
+
+        
     }
 
-    // Set the solution
-    solution.setBins(bins);
-    solution.setBinNum(binNum);
-    cout << "gap" << solution.getAbsGap() << endl;
+    // cout << "Bin num: " << binNum << endl;
+    solution.setUFBins(ufBins);
+    solution.setFBins(fBins);
 
     return solution;
 }
 
 
+// Function to generate solution with MBS heuristic 
+// Solution MBSHeuristic::genSolution(const Problem& problem){
+//     vector<Bin> unfilledBins;
+//     vector<Bin> filledBins;
+
+
+//     // Initialize the solution
+//     Solution solution(problem);
+
+//     // Get the items in the problem
+//     vector<Item> items = problem.getItems();
+
+//     // Initialize the unpacked items
+//     vector<Item> uItems = items;
+
+//     // Sort the unpacked items in a descending order of volumn
+//     sort(uItems.begin(), uItems.end(), Heuristic::compareItem);
+
+//     // Pack the items according to MBS until all items are packed
+//     int binNum = 0;
+//     while(uItems.size() > 0){
+//         Bin bin(binNum, problem.getBinCap());
+//         binNum++;
+
+//         map<int, vector<Item>> maxPacking = findMaxPack(bin.getCapLeft(), uItems);
+//         auto it = maxPacking.begin();
+//         int maxVol = it->first;
+//         vector<Item> pItems = it->second;
+//         bin.setCapLeft(bin.getCapLeft() - maxVol);
+//         for(Item item : pItems){
+//             bin.addItem(item);
+
+// 	        auto it2 = find(uItems.begin(), uItems.end(), item);
+// 	        int index = it2 - uItems.begin();
+// 	        uItems.erase(uItems.begin() + index);
+//         }
+
+//         bins.push_back(bin);
+//     }
+
+//     // Set the solution
+//     solution.setBins(bins);
+//     solution.setBinNum(binNum);
+//     cout << "gap" << solution.getAbsGap() << endl;
+
+//     return solution;
+// }
+
+
 // Function to find the maximum packing given a restricted capacity and a vector of unpacked items in a descending order of volumn
 // Return the map of the total volumn and the items in the packing
-map<int, vector<Item>> MBSHeuristic::findMaxPack(int cap, vector<Item> uItems){
-    int i;
+// map<int, vector<Item>> MBSHeuristic::findMaxPack(int cap, vector<Item> uItems){
+//     int i;
 
-    // Find the index of item with the maximum volumn and is smaller than the capacity
-    for(i = 0; i < uItems.size(); i++){
-        if(uItems[i].getVol() <= cap){
-            break;
-        }
-    }
+//     // Find the index of item with the maximum volumn and is smaller than the capacity
+//     for(i = 0; i < uItems.size(); i++){
+//         if(uItems[i].getVol() <= cap){
+//             break;
+//         }
+//     }
 
-    // Base case
-    if(i == uItems.size()){ // If there is no such item, return the map of volumn 0 and the vector containing no item
-        vector<Item> retItem;
-        map<int, vector<Item>> ret = {{0, retItem}};
-        return ret;
-    }else if(i == uItems.size() - 1){ // If the item is the last one in the vector, return the map of its volumn and the vector containing it
-        vector<Item> retItem{uItems[i]};
-        map<int, vector<Item>> ret = {{uItems[i].getVol(), retItem}};
-        return ret;
-    }
+//     // Base case
+//     if(i == uItems.size()){ // If there is no such item, return the map of volumn 0 and the vector containing no item
+//         vector<Item> retItem;
+//         map<int, vector<Item>> ret = {{0, retItem}};
+//         return ret;
+//     }else if(i == uItems.size() - 1){ // If the item is the last one in the vector, return the map of its volumn and the vector containing it
+//         vector<Item> retItem{uItems[i]};
+//         map<int, vector<Item>> ret = {{uItems[i].getVol(), retItem}};
+//         return ret;
+//     }
 
-    // Recursive case
-    // If the current item is chosen
-    map<int, vector<Item>> map1 = findMaxPack(cap - uItems[i].getVol(), vector<Item>(uItems.begin() + 1, uItems.end()));
-    auto it1 = map1.begin();
-    int vol1 = it1->first + uItems[i].getVol();
-    vector<Item> retItems = it1->second;
-    vector<Item> pItems1{uItems[i]};
-    pItems1.insert(pItems1.end(), retItems.begin(), retItems.end());
-    // If the current item is not chosen
-    map<int, vector<Item>> map2 = findMaxPack(cap, vector<Item>(uItems.begin() + 1, uItems.end()));
-    auto it2 = map2.begin();
-    int vol2 = it2->first;
-    vector<Item> pItems2 = it2->second;
+//     // Recursive case
+//     // If the current item is chosen
+//     map<int, vector<Item>> map1 = findMaxPack(cap - uItems[i].getVol(), vector<Item>(uItems.begin() + 1, uItems.end()));
+//     auto it1 = map1.begin();
+//     int vol1 = it1->first + uItems[i].getVol();
+//     vector<Item> retItems = it1->second;
+//     vector<Item> pItems1{uItems[i]};
+//     pItems1.insert(pItems1.end(), retItems.begin(), retItems.end());
+//     // If the current item is not chosen
+//     map<int, vector<Item>> map2 = findMaxPack(cap, vector<Item>(uItems.begin() + 1, uItems.end()));
+//     auto it2 = map2.begin();
+//     int vol2 = it2->first;
+//     vector<Item> pItems2 = it2->second;
 
-    // Return the case with larger total volumn
-    if(vol1 >= vol2){
-        map<int, vector<Item>> ret = {{vol1, pItems1}};
-        return ret;
-    }else{
-        map<int, vector<Item>> ret = {{vol2, pItems2}};
-        return ret;
-    }
-}
+//     // Return the case with larger total volumn
+//     if(vol1 >= vol2){
+//         map<int, vector<Item>> ret = {{vol1, pItems1}};
+//         return ret;
+//     }else{
+//         map<int, vector<Item>> ret = {{vol2, pItems2}};
+//         return ret;
+//     }
+// }
 
 
 
@@ -536,7 +620,7 @@ map<int, vector<Item>> MBSHeuristic::findMaxPack(int cap, vector<Item> uItems){
 
 // Constructor and destructor
 Solver::Solver(){
-    heuristic = new MBSHeuristic();
+    heuristic = new BFHeuristic();
 }
 Solver::~Solver(){
     delete heuristic;
